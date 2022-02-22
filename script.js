d0569667a13216c975d67828e9db8eb6e5cc4796
“Christina <christinaasquith@gmail.com>
2022-02-22 08:09:51 -0600

    initial code

---
+++ script.js
@@ -0,0 +1,25 @@
+/*Practicing some Kwk stuff that's confusing me: 
+var favHtml = "divs";
+console.log(`writing the ${favHtml}`);
+var favCss = "color";
+var favJavascript = "functions";
+var favGithub = "repos";*/
+
+var name ="Karlie";
+var tinsOfKookies =1200;
+var mealsDonatedPerTin =10;
+var mealsDonated =tinsOfKookies * mealsDonatedPerTin;
+console.log(mealsDonated);
+
+var campersPerCamp ="23.5";
+var klossyCamps ="36";
+var klossyCampers = campersPerCamp * klossyCamps;
+console.log(klossyCampers); 
+
+var queen ="Beyonce";
+if (queen ==="Beyonce"){
+    alert("You are correct!")
+
+} else {
+    alert("Beyonce is the only queen")
+}
